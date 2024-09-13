92a66d006ffe4752a07f039884e623f4e9057913
Bruno Esquiva <brunoesquiva1211@gmail.com>
2024-09-13 13:29:04 +0200

    PortFolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { CommonModule } from '@angular/common';
+import { TecnologiasComponent } from './tecnologias/tecnologias.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, AboutComponent, CommonModule, TecnologiasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
