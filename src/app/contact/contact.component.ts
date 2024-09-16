544ca0bf92b9d2f84dabd29b91ec65289b7b92c9
Bruno Esquiva <brunoesquiva1211@gmail.com>
2024-09-16 13:56:25 +0200

    HECHO

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  public sendEmail(e: Event) {
+    e.preventDefault();
 
+    emailjs
+      .sendForm('service_s2lxkrr', 'template_slh1pe5', e.target as HTMLFormElement, {
+        publicKey: 'nqm-cCukCC6stYngA',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
 }
